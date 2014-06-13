ab08a7285f08b028311549bf82bdca28f2709898
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2014-06-13 23:59:54 +0100

    added everything

--- suprsub-placeholder.js
+++ suprsub-placeholder.js
@@ -17,15 +17,12 @@ if (Meteor.isClient) {
         onChanged: changedCallback,
         autoplaySpeed: 500
       });
+      $('.signup .button').click(function() {
+        templateAttach(Template.signupModalHolder, function() {
+          modalOptions(); $('.ui.modal').modal('show');
+        });        
+      });
     });
-    (function(d,s,id) {
-      var js,fjs=d.getElementsByTagName(s)[0];
-      if(!d.getElementById(id)) {
-        js=d.createElement(s);
-        js.id=id;js.src="//platform.twitter.com/widgets.js";
-        fjs.parentNode.insertBefore(js,fjs);
-      }
-    })(document,"script","twitter-wjs");
     (function(d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
@@ -37,10 +34,99 @@ if (Meteor.isClient) {
 
   Template.parallaxBox.rendered = function() {
     $.stellar();
-  }
+  };
+
+  Template.dynamicTemplate.chooseTemplate = function (name) {
+    return {template: Template[name]};
+  };
+
+  Template.signupModal.events({
+    'click #signup-button': function() {
+      Meteor.call('userCreate', $('#signup-email').val(), function(err, res) {
+        if (err) console.log(err);
+        if (res) {
+          $('#signup-email').css('color', 'hsl(120, 75%, 30%)');
+          Meteor.setTimeout(function() {
+            $('.ui.modal').modal('hide');
+          }, 1250);
+        }
+        else {
+          $('#signup-email').css('color', 'red');
+          Meteor.setTimeout(function() {
+            $('#signup-email').css('color', 'rgba(0, 0, 0, 0.701961)');           
+          }, 1250);         
+        }
+      });
+    },
+    'click #twitter-login': function() {
+      Meteor.loginWithTwitter({}, function() {
+        Meteor.setTimeout(function() {
+          $('.ui.modal').modal('hide');
+        }, 750);       
+      });
+    },
+    'click #facebook-login': function() {
+      Meteor.loginWithFacebook({}, function() {
+        Meteor.setTimeout(function() {
+          $('.ui.modal').modal('hide');
+        }, 750);
+      });
+    }
+  });
+
+  templateAttach = function(template, callback, data) {
+    if (typeof template === "string") template = Template[template];
+    if (!template) return false;
+    if (data)
+      UI.insert(UI.renderWithData(template, data), document.body);
+    else
+      UI.insert(UI.render(template), document.body);
+    return callback && callback.apply(this, arguments);
+  };
+}
+
+function modalOptions() {
+  $('.ui.modal').modal({
+    onHide: function() {
+      $('.ui.dimmer.page').remove();
+      $('.ui.modal').remove();
+    }
+  });
 }
 
 if (Meteor.isServer) {
+
+  SecureData = new Meteor.Collection('securedata');
+
   Meteor.startup(function () {
+
+    facebookDetails = SecureData.findOne({name: 'facebookDetails'});
+    twitterDetails = SecureData.findOne({name: 'twitterDetails'});
+
+    Accounts.config({
+      sendVerificationEmail: false,
+      forbidClientAccountCreation: false
+    });
+
+    Accounts.loginServiceConfiguration.remove({
+      service: "facebook"
+    });
+    Accounts.loginServiceConfiguration.remove({
+      service: "twitter"
+    });
+    if (Meteor.absoluteUrl().slice(0,22) !== "http://localhost:3000/") {
+      Accounts.loginServiceConfiguration.insert(facebookDetails);
+      Accounts.loginServiceConfiguration.insert(twitterDetails);
+    }
+
+    Meteor.methods({
+      userCreate: function(email) {
+        Accounts.createUser({email: email});
+        return true;
+      }
+    });
+
   });
 }
+
+
